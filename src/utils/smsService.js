f4f7781adcd26d1ce1f3903d10873df81cc2aa5f
Pankaj Patil <pankajpatil4949@gmail.com>
2025-03-25 23:35:16 +0530

    message update

--- src/utils/smsService.js
+++ src/utils/smsService.js
@@ -10,7 +10,7 @@ export default async function SendWhatsAppNotification(userDetails){
         const response = await client.messages.create({
         from: 'whatsapp:+14155238886',
         to: 'whatsapp:+918108300397',
-        body: `Enquiry received from [${userDetails.phone}]. \nDetails-\n\tName: ${userDetails.name},\n\tEmail: ${userDetails.email},\n\tAddress: ${userDetails.address},\n\tModel: ${userDetails.model}.\n\t${userDetails.message}`
+        body: `Enquiry received from [${userDetails.phone}]. \nDetails-\n\tName: ${userDetails.name},\n\tEmail: ${userDetails.email},\n\tAddress: ${userDetails.address},\n\tModel: ${userDetails.model}.\n\tMessage: ${userDetails.message}`
         })
 
         logger.info(`Enquiry received from [${userDetails.phone}]. \nDetails-\n\tName: ${userDetails.name},\n\tEmail: ${userDetails.email},\n\tAddress: ${userDetails.address},\n\tModel: ${userDetails.model}.\n\t${userDetails.message}`);
